840fed8e320105ccae5ecafb55a1f96eb504c651
ytkalan <atomyuen95@gmail.com>
2018-10-31 03:06:30 +0800

    Update logistic_model

---
+++ logistic_model.py
@@ -0,0 +1,30 @@
+from trend_data_generator import TrendDataGenerator
+from sklearn.linear_model import *
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+
+years=[2012, 2013, 2014, 2015, 2016]
+windows=[1, 2, 3, 4, 5]
+
+for year in years:
+    for window in windows:
+        label, data = TrendDataGenerator(
+            read_from_raw_file=False,
+            usable_year=year,
+            window=window
+        ).get_trend_data()
+
+        parameters = {	
+            'penalty': ['l2'],
+            'tol' : [1e-4, 1e-5, 1e-8],
+            'C': [0.05, 0.2, 0.5, 1],
+            'solver': ['newton-cg', 'lbfgs', 'sag', 'saga'],
+            'warm_start': [True, False]
+        }
+
+        lm_model = LogisticRegression(multi_class='multinomial', max_iter=5000)
+
+        grid_logistic = GridSearchCV(lm_model, parameters, cv=5)
+        grid_logistic.fit(data, label)
+
+        pd.DataFrame(grid_logistic.cv_results_).sort_values(by='rank_test_score').to_csv('Logistic-{0}-{1}.csv'.format(year, window))
