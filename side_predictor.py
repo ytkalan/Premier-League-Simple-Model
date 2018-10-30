9ac1fb33fedb5a770aa77af20324a98832cef32d
ytkalan <atomyuen95@gmail.com>
2018-10-31 02:57:19 +0800

    Update param of side_predictor.py

--- side_predictor.py
+++ side_predictor.py
@@ -1,7 +1,11 @@
 import pickle
 import numpy as np
-from sklearn.linear_model import *
 from trend_data_generator import TrendDataGenerator
+from sklearn.linear_model import *
+from sklearn.ensemble import *
+from sklearn.neural_network import *
+from sklearn.svm import *
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 # Get data from file
 trend_data = TrendDataGenerator(
@@ -13,10 +17,14 @@ training_label, training_data = trend_data.get_trend_data()
 
 # define and train model
 lm_model = LogisticRegression(
-    max_iter=50000,
-    class_weight="balanced",
-    solver="saga",
-    multi_class="multinomial"
+    C=1,
+    penalty='l2',
+    solver='saga',
+    tol=1e-08,
+    warm_start=False,
+    class_weight = "balanced",
+    multi_class='multinomial',
+    max_iter=10000
 )
 lm_model.fit(training_data, training_label)
 
