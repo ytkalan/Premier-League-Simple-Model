9cb44c1acd701b0a63db410fbe6e505ddac94c3b
ytkalan <atomyuen95@gmail.com>
2018-11-01 00:58:42 +0800

    Update odd_scraper

--- scraper/odd_scraper.js
+++ scraper/odd_scraper.js
@@ -1,10 +1,26 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'hn.pdf', format: 'A4'});
+const scrape = async (url) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const result = [];
+    await page.goto(url, {waitUntil: 'networkidle2'});
+    await page.waitForSelector('#col-content > #tournamentTable > #tournamentTable > tbody')
+    const data = await page.evaluate(() => {
+        const rows = Array.from(document.querySelectorAll(
+            '#col-content > #tournamentTable > #tournamentTable > tbody > .deactivate'
+        ))
+        return rows.map(td => td.innerHTML)
+      });
+    await browser.close();
+    return data;
+};
 
-  await browser.close();
-})();
+scrape('https://www.oddsportal.com/soccer/england/premier-league-2015-2016/results/#/page/1/').then((value) => {
+    const result = value.map(data => {
+        const regex = RegExp('odds_text');
+        return regex.exec(data);
+    });
+    console.log(result);
+    console.log(result.length);
+});
